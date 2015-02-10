e5e3363190cd8115d1d9779e762679fb543e400b
liufei_it <liufei_it@126.com>
2015-02-10 11:30:41 +0800

    shell

--- sso/build_aliyun.sh
+++ sso/build_aliyun.sh
@@ -2,5 +2,5 @@
 
 mvn clean && mvn install  -Dmaven.test.skip=true
 echo "build success"
-scp -r api/target/sso-api*.war root@aliyun:~/jetty/app/
+scp -r api/target/sso-api*.war root@aliyun:~/jetty/apps/
 echo "deploy success"
